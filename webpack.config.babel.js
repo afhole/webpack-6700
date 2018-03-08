b932816233c1c684aeba4cf6f78f6a7d2b180326
Alastair Hole <alastair.hole@futurenet.com>
2018-03-08 17:23:16 +0000

    Disable babel-loader

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,21 +1,21 @@
 export default {
-		module: {
-		rules: [
-			{
-				test: /\.js$/,
-				//Exclude json.js to simulate importing JSON from an NPM module
-				exclude: /(node_modules)|(json.js)/,
-				// exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true,
-						presets: [
-							['env', { 'modules': false }]
-						]
-					}
-				}
-			}
-		]
-	}
+	// 	module: {
+	// 	rules: [
+	// 		{
+	// 			test: /\.js$/,
+	// 			//Exclude json.js to simulate importing JSON from an NPM module
+	// 			exclude: /(node_modules)|(json.js)/,
+	// 			// exclude: /(node_modules)/,
+	// 			use: {
+	// 				loader: 'babel-loader',
+	// 				options: {
+	// 					cacheDirectory: true,
+	// 					presets: [
+	// 						['env', { 'modules': false }]
+	// 					]
+	// 				}
+	// 			}
+	// 		}
+	// 	]
+	// }
 };
